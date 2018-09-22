841be19d179805e1cbdae284a16f81014152215b
Teittinen Topias <tote@lx7-fuxi351.ad.helsinki.fi>
2018-09-23 00:59:29 +0300

    passwords and slight tinkering

--- Gemfile
+++ Gemfile
@@ -41,6 +41,8 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'rubocop'
 
+gem 'bcrypt', '~> 3.1.7'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,27 @@
 class User < ApplicationRecord
     include RatingAverage
 
-    validates :username, uniqueness: true, length: { minimum: 3, maximum: 30 }
+    has_secure_password
 
-    has_many :ratings
+    validates :username, uniqueness: true, length: { minimum: 3, maximum: 30 }
+    validates :password, length: {minimum: 4}
+    validate :password_contains_uppercase_char
+    validate :password_contains_number
+
+    has_many :ratings, dependent: :destroy
     has_many :beers, through: :ratings
     has_many :memberships, dependent: :destroy
     has_many :beer_clubs, through: :memberships
+
+    def password_contains_uppercase_char
+        if not password =~ /[A-Z]/
+            errors.add(:password, "password must contain at least one upper case character")
+        end
+    end
+
+    def password_contains_number
+        if not password =~ /[0-9]/
+            errors.add(:password, "password must contain at least one number")
+        end
+    end
 end
