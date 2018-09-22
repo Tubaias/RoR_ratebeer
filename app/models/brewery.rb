7f89fec7f41b38975ce8fdb46b0c7934821159a2
Teittinen Topias <tote@lx7-fuxi351.ad.helsinki.fi>
2018-09-22 22:26:50 +0300

    users, validation and other improvements

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,8 +1,18 @@
 class Brewery < ApplicationRecord
     include RatingAverage
 
+    validates :name, presence: true
+    validates :year, numericality: { greater_than_or_equal_to: 1040, only_integer: true }
+    validate :year_cannot_be_in_the_future
+
     has_many :beers, dependent: :destroy
     has_many :ratings, through: :beers
+
+    def year_cannot_be_in_the_future
+        if year > Time.now.year
+            errors.add(:year, "can't be in the future")
+        end
+    end
 
     def print_report
         puts name
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   root 'breweries#index'
+  resources :users
   resources :beers
   resources :breweries
   resources :ratings, only: [:index, :new, :create, :destroy]
-
+  resource :session, only: [:new, :create, :destroy]
+  
+  get 'signup', to: 'users#new'
+  get 'signin', to: 'sessions#new'
+  delete 'signout', to: 'sessions#destroy'
   get 'kaikki_bisset', to: 'beers#index'
 end
