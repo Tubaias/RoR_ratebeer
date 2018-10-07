194996705b9d853884030418d4fe75958517861c
Teittinen Topias <tote@lx7-fuxi351.ad.helsinki.fi>
2018-10-07 18:37:07 +0300

    style as a separate table part 1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_22_205237) do
+ActiveRecord::Schema.define(version: 2018_10_07_153209) do
 
   create_table "beer_clubs", force: :cascade do |t|
     t.string "name"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2018_09_22_205237) do
 
   create_table "beers", force: :cascade do |t|
     t.string "name"
-    t.string "style"
+    t.string "old_style"
     t.integer "brewery_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -48,6 +48,13 @@ ActiveRecord::Schema.define(version: 2018_09_22_205237) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+  end
+
+  create_table "styles", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
